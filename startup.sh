d50c292d8371187d1e832af82a8dd6c2f8a04af9
palmerlovett <palmerlovett@me.com>
2026-09-18 07:09:22 +0000

    another edit to startup mindful now of environments

--- startup.sh
+++ startup.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -euo pipefail
 cd "$(dirname "$0")"
-exec gunicorn django_project.wsgi:application \
+exec .venv/bin/gunicorn django_project.wsgi:application \
   --bind 127.0.0.1:8010
