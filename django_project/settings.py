7bdecf89e8984912c6c14bfcc4658c567d9a6965
palmerlovett <palmerlovett@me.com>
2026-09-18 07:29:34 +0000

    fixed egregious spelling error

--- django_project/settings.py
+++ django_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [".replit.dev", ".replit.app", ".lastman.enterpises"]
+ALLOWED_HOSTS = [".replit.dev", ".replit.app", ".lastman.enterprises"]
 CSRF_TRUSTED_ORIGINS = ["https://*.replit.dev", "https://*.replit.app", "https://*.lastman.enterprises"]
 
 # Application definition
